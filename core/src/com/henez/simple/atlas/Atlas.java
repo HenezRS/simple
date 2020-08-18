18921bd4e3eb1b67575844186b10e67f5a2a8cda
Henry <henrico789@gmail.com>
2020-08-18 13:21:07 +1000

    gradle 6.6, jdk 11

--- core/src/com/henez/simple/atlas/Atlas.java
+++ core/src/com/henez/simple/atlas/Atlas.java
@@ -5,15 +5,14 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Atlas {
-    private static Texture textureTiles;
     private static TextureRegion[][] textureRegionTiles;
 
     public static void load() {
-        textureTiles = new Texture(Gdx.files.internal("png/tiles.png"));
+        Texture textureTiles = new Texture(Gdx.files.internal("png/tiles.png"));
         textureRegionTiles = TextureRegion.split(textureTiles, 16, 16);
-        for (int j = 0; j < textureRegionTiles.length; ++j) {
-            for (int i = 0; i < textureRegionTiles[j].length; ++i) {
-                textureRegionTiles[j][i].flip(false, true);
+        for (TextureRegion[] textureRegionTile : textureRegionTiles) {
+            for (TextureRegion textureRegion : textureRegionTile) {
+                textureRegion.flip(false, true);
             }
         }
     }
